5d401ad005bb8b33830a7f0cf8a37d5d3ea0e18f
coderNet2021 <cnss.saida@gmail.com>
2023-08-21 23:55:14 +0200

    blog and archive implementation

---
+++ src/app/archive/archive.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-archive',
+  templateUrl: './archive.component.html',
+  styleUrls: ['./archive.component.css']
+})
+export class ArchiveComponent implements OnInit {
+  year:number=0;
+  month :number=0;
+
+  constructor(private router : Router,private route: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+    let params =this.route.snapshot.paramMap;
+    this.year = Number(params.get('year'));
+    this.month = Number(params.get('month'));
+  }
+
+  viewAll(){
+this.router.navigate(['/']);
+  }
+
+}
